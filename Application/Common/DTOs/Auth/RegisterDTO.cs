bb47a80b4111bfae40ee071cd239acdf4c9d48e4
mahmudmahim <mahmud082543@gmail.com>
2025-12-04 01:25:23 +0600

    SignUpDoneWithProperDatabaseMigration

---
+++ Application/Common/DTOs/Auth/RegisterDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseRentalApplication.Common.DTOs.Auth
+{
+    public class RegisterDTO
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Phone { get; set; }
+        public string? Email { get; set; }
+        public string? Address { get; set; }
+        public string? NIDNo { get; set; }
+        public string? Password { get; set; }
+        public bool IsOwner { get; set; }
+        public bool IsAdmin { get; set; }
+
+    }
+}
---
+++ Application/Common/Interfaces/Auth/IAuthService.cs
@@ -0,0 +1,16 @@
+﻿using HouseRentalApplication.Common.DTOs.Auth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseRentalApplication.Common.Interfaces.Auth
+{
+    public interface IAuthService
+    {
+        Task<AuthResponseDTO> RegisterAsync(RegisterDTO model);
+        Task<AuthResponseDTO> LoginAsync(LoginDTO model);
+        Task<bool> AssignRoleAsync(string userId, string role);
+    }
+}
