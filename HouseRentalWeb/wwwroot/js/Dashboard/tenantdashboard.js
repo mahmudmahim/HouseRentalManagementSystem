91e629428556015fca83c0bc82088a9cf42b7d6b
mahmud <dev16.pintech@gmail.com>
2025-12-12 04:44:05 +0600

    RequestSendAdded

---
+++ HouseRentalWeb/wwwroot/js/Dashboard/tenantdashboard.js
@@ -0,0 +1,9 @@
+﻿$("#sentRequest").on("click", function () {
+    toastr.success("Request Send Successful!");
+});
+
+$("#btnLogout").click(function () {
+    localStorage.removeItem("token");
+    localStorage.removeItem("role");
+    window.location.href = "/Auth/Login";
+});
